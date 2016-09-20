af3c7c7161c7ce3a48aacdc282ba8ecb1a20e096
SaWinchester <samukasaww@live.com>
2016-09-20 19:41:59 -0300

    Atualização

--- jobaiml.py
+++ jobaiml.py
@@ -2,7 +2,7 @@
 import aiml
 ai = aiml.Kernel() # inicialização
 ai.learn('std-startup.xml') # lê o arquivo principal da AIML e faz referências aos outros
-ai.respond('LOAD AIML B') # faz com que os outros arquivos da AIML sejam carregados
+ai.respond('s') # faz com que os outros arquivos da AIML sejam carregados
 
 def responde_mensagem(mensagem):
 	print mensagem
