c0e34fde43860cd8f70cc513e334cc3dcb649bea
SaWinchester <samukasaww@live.com>
2016-09-20 20:00:15 -0300

    Atualização

--- jobaiml.py
+++ jobaiml.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 import aiml
-ai = aiml.Kernel() # inicialização
-ai.learn('*.aiml') # lê o arquivo principal da AIML e faz referências aos outros
+#ai = aiml.Kernel() # inicialização
+#ai.learn('*.aiml') # lê o arquivo principal da AIML e faz referências aos outros
 #ai.respond('s') # faz com que os outros arquivos da AIML sejam carregados
 
 def responde_mensagem(mensagem):
+	ai = aiml.Kernel() # inicialização
+	ai.learn('*.aiml') # lê o arquivo principal da AIML e faz referências aos outros
 	print mensagem
 	resposta = ai.respond(mensagem)
 	print resposta
