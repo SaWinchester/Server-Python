69f2e59b741d8611d08ecd4824f0012633cc7b62
Samuel Gonçalves Cunha <samukasaww@live.com>
2016-09-20 08:41:28 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,8 +5,8 @@ from server import app as application, run
 
 if __name__ == '__main__':
 
-	ip = os.environ.get('OPENSHIFT_PYTHON_IP', '192.168.0.104')
-	port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 60026))
+	ip = '0.0.0.0'
+	port = int(80)
 
     # Para detalhae melhor os Logs no OpenShift
 	application.config['PROPAGATE_EXCEPTIONS'] = True
