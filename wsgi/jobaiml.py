f3d830875845139359ff16aa7b04c4a4f1c8d235
SaWinchester <samukasaww@live.com>
2016-09-26 16:34:20 -0300

    Atualizando repo

--- wsgi/jobaiml.py
+++ wsgi/jobaiml.py
@@ -2,16 +2,17 @@
 import aiml
 import os
 
-ai = aiml.Kernel() # inicialização
 
-if os.path.isfile("bot_brain.brn"):
-    ai.bootstrap(brainFile = "bot_brain.brn")
-else:
-    ai.bootstrap(learnFiles = "std-startup.xml", commands = "LOAD AIML B")
-    #ai.saveBrain("bot_brain.brn")
 
 def responde_mensagem(mensagem):
-	global ai
+
+	ai = aiml.Kernel() # inicialização
+
+	#if os.path.isfile("bot_brain.brn"):
+	ai.bootstrap(brainFile = "bot_brain.brn")
+	#else:
+	#	ai.bootstrap(learnFiles = "std-startup.xml", commands = "LOAD AIML B")
+
 	print mensagem
 	resposta = ai.respond(mensagem)
 	print resposta
