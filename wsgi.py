a44353c08a608dcc392a9f08a7a0b31510e0b08e
SaWinchester <samukasaww@live.com>
2016-09-26 14:48:33 -0300

    Atualizando repo

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
 #coding: utf-8
 
+#!/usr/bin/python
+import os
+import sys
+
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
