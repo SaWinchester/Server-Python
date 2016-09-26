f371595a0fb8caf5ad4d9ad2292e49ea942c3f56
SaWinchester <samukasaww@live.com>
2016-09-26 16:46:31 -0300

    Atualizando repo

--- wsgi/jobaiml.py
+++ wsgi/jobaiml.py
@@ -2,7 +2,7 @@
 import aiml
 
 ai = aiml.Kernel() # inicialização
-ai.learn('$OPENSHIFT_DATA_DIR') # lê o arquivo principal da AIML e faz referências aos outros
+ai.learn('$OPENSHIFT_DATA_DIR/*') # lê o arquivo principal da AIML e faz referências aos outros
 
 def responde_mensagem(mensagem):
 	print mensagem
