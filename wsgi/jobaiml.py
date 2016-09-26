30e097d31a2a8c429d6daaa2253b4288b834933a
SaWinchester <samukasaww@live.com>
2016-09-26 16:41:41 -0300

    Atualizando repo

--- wsgi/jobaiml.py
+++ wsgi/jobaiml.py
@@ -1,18 +1,10 @@
 # coding: utf-8
 import aiml
-import os
 
-
+ai = aiml.Kernel() # inicialização
+ai.learn('$OPENSHIFT_DATA_DIR') # lê o arquivo principal da AIML e faz referências aos outros
 
 def responde_mensagem(mensagem):
-
-	ai = aiml.Kernel() # inicialização
-
-	#if os.path.isfile("bot_brain.brn"):
-	ai.bootstrap(brainFile = "bot_brain.brn")
-	#else:
-	#	ai.bootstrap(learnFiles = "std-startup.xml", commands = "LOAD AIML B")
-
 	print mensagem
 	resposta = ai.respond(mensagem)
 	print resposta
